e055c11c78ffa2c1b052920289315fcd18ebfb25
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2024-03-25 22:28:21 +0700

    commit

--- app/src/main/java/com/example/dogapp/view/ListFragment.java
+++ app/src/main/java/com/example/dogapp/view/ListFragment.java
@@ -11,13 +11,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 import com.example.dogapp.R;
 import com.example.dogapp.model.DogBreed;
 import com.example.dogapp.viewmodel.DogsAdapter;
 import com.example.dogapp.viewmodel.DogsApiService;
+import com.google.android.material.search.SearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +41,7 @@ public class ListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         if (getArguments() != null) {
 
         }
@@ -79,5 +84,24 @@ public class ListFragment extends Fragment {
                         Log.d("DEBUG","Fail");
                     }
                 });
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_search,menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.mi_search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener) {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                dogsAdapter.getFilter().filter(s);
+                return false;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/dogapp/viewmodel/DogsAdapter.java
+++ app/src/main/java/com/example/dogapp/viewmodel/DogsAdapter.java
@@ -1,9 +1,14 @@
 package com.example.dogapp.viewmodel;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,13 +23,16 @@ import com.example.dogapp.model.DogBreed;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class DogsAdapter extends RecyclerView.Adapter<DogsAdapter.ViewHolder> {
+public class DogsAdapter extends RecyclerView.Adapter<DogsAdapter.ViewHolder> implements Filterable {
 
     private ArrayList<DogBreed> dogBreeds;
+    private ArrayList<DogBreed> dogBreedsCopy;
 
     public DogsAdapter(ArrayList<DogBreed> dogBreeds) {
         this.dogBreeds = dogBreeds;
+        this.dogBreedsCopy = dogBreeds;
     }
 
     @NonNull
@@ -52,6 +60,35 @@ public class DogsAdapter extends RecyclerView.Adapter<DogsAdapter.ViewHolder> {
         return dogBreeds.size();
     }
 
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String input = charSequence.toString().toLowerCase();
+                List<DogBreed> filteredDog = new ArrayList<DogBreed>();
+                if(input.isEmpty()) {
+                    filteredDog.addAll(dogBreedsCopy);
+                }else {
+                    for (DogBreed dog: dogBreedsCopy) {
+                        filteredDog.add(dog);
+                    }
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = filteredDog;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults filterResult) {
+                dogBreeds = new ArrayList<>();
+                dogBreeds.addAll((List)filterResult.values);
+                notifyDataSetChanged();
+            }
+        };
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public DogsItemBinding binding;
@@ -60,9 +97,9 @@ public class DogsAdapter extends RecyclerView.Adapter<DogsAdapter.ViewHolder> {
             super(itemBinding.getRoot());
             this.binding = itemBinding;
 
-            binding.ivImagedog.setOnClickListener(new View.OnClickListener() {
+            itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
+                public void onClick(View view) {
                     DogBreed dog = dogBreeds.get(getAdapterPosition());
                     Bundle bundle = new Bundle();
                     bundle.putSerializable("dogBreed",dog);
@@ -70,6 +107,85 @@ public class DogsAdapter extends RecyclerView.Adapter<DogsAdapter.ViewHolder> {
 
                 }
             });
+
+            itemView.setOnTouchListener(new OnSwipeTouchListener() {
+                @Override
+                public void onSwipeLeft() {
+                    if(binding.layout1.getVisibility() == View.GONE) {
+                        binding.layout1.setVisibility(View.VISIBLE);
+                        binding.layout2.setVisibility(View.GONE);
+
+                    }else {
+                        binding.layout1.setVisibility(View.GONE);
+                        binding.layout2.setVisibility(View.VISIBLE);
+                    }
+                    super.onSwipeLeft();
+                }
+            });
+        }
+    }
+
+    public class OnSwipeTouchListener implements View.OnTouchListener {
+
+        private GestureDetector gestureDetector = new GestureDetector(new GestureListener());
+
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return gestureDetector.onTouchEvent(event);
+        }
+
+        private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
+
+            private static final int SWIPE_THRESHOLD = 100;
+            private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+
+            // Determines the fling velocity and then fires the appropriate swipe event accordingly
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                boolean result = false;
+                try {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    if (Math.abs(diffX) > Math.abs(diffY)) {
+                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffX > 0) {
+                                onSwipeRight();
+                            } else {
+                                onSwipeLeft();
+                            }
+                        }
+                    } else {
+                        if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffY > 0) {
+                                onSwipeDown();
+                            } else {
+                                onSwipeUp();
+                            }
+                        }
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+                return result;
+            }
+        }
+
+        public void onSwipeRight() {
+        }
+
+        public void onSwipeLeft() {
+        }
+
+        public void onSwipeUp() {
+        }
+
+        public void onSwipeDown() {
         }
     }
 }
